bf8352ba5436d76649ced8be69b1ccfb18923e7d
KácioSilva <101821745+KacioSilva@users.noreply.github.com>
2023-12-02 16:24:19 -0300

    update

---
+++ aplicativo/api.py
@@ -0,0 +1,20 @@
+from flask import Flask, request, jsonify
+from aplicativo.rabbit import RabbitMQ
+
+app = Flask(__name__)
+rabbit = RabbitMQ('chat_queue')
+rabbit.start_consuming()  # Iniciar a thread de consumo
+
+@app.route('/send_message', methods=['POST'])
+def send_message():
+    data = request.get_json()
+    message = data.get('message')
+    rabbit.send_message(message)
+    return jsonify({'status': 'Message sent successfully'})
+
+@app.route('/receive_messages', methods=['GET'])
+def receive_messages():
+    with rabbit.lock:
+        messages = list(rabbit.messages)
+        rabbit.messages = []  # Limpar as mensagens recebidas
+    return jsonify({'messages': messages})
