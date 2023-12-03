bda8091823a8a3ab14de20093c6e251ce629876a
KácioSilva <101821745+KacioSilva@users.noreply.github.com>
2023-12-03 00:54:08 -0300

    update

--- app.py
+++ app.py
@@ -1,24 +1,42 @@
+# backend/app.py
 from flask import Flask, request, jsonify
-from rabbit import RabbitMQ
-import time
-
 
 app = Flask(__name__)
-rabbit = RabbitMQ('chat_queue')
-
-time.sleep(10)
-rabbit.start_consuming()  # Iniciar a thread de consumo
+user_messages = {}  # Dicionário para armazenar mensagens por usuário
 
 @app.route('/send_message', methods=['POST'])
 def send_message():
-    data = request.get_json()
-    message = data.get('message')
-    rabbit.send_message(message)
-    return jsonify({'status': 'Message sent successfully'})
+    data = request.json
+    user_id = data.get('user_id')
+    message_content = data.get('message')
 
-@app.route('/receive_messages', methods=['GET'])
-def receive_messages():
-    with rabbit.lock:
-        messages = list(rabbit.messages)
-        rabbit.messages = []  # Limpar as mensagens recebidas
-    return jsonify({'messages': messages})
+    if user_id is None or message_content is None:
+        return {'status': 'error', 'message': 'User ID and message are required.'}
+
+    if user_id not in user_messages:
+        user_messages[user_id] = []
+
+    new_message = {'content': message_content, 'read': False}
+    user_messages[user_id].append(new_message)
+
+    return {'status': 'success'}
+
+@app.route('/get_messages/<user_id>', methods=['GET'])
+def get_messages(user_id):
+    if user_id not in user_messages:
+        return {'status': 'error', 'message': 'User not found.'}
+
+    # Recupera as mensagens do usuário
+    messages = user_messages[user_id]
+
+    # Marca as mensagens como lidas
+    for message in messages:
+        message['read'] = True
+
+    # Esvazia a lista de mensagens para o usuário
+    user_messages[user_id] = []
+
+    return jsonify(messages)
+
+if __name__ == '__main__':
+    app.run(debug=True)
